ed742a22d7032dbf51a7c3ab58fe8b7e3455c74f
ratul <ratuljh@gmail.com>
2018-04-11 23:50:52 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+search = input("Enter search query: ")
+params = {"q": search}
+
+r = requests.get("https://www.bing.com/search", params=params)
+
+soup = BeautifulSoup(r.text)
+
+f = open("soup.html", "w+")
+f.write(soup.prettify())
+
+print(soup.prettify())
