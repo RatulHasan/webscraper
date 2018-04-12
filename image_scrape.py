5ce36918002f345d173bac01205b65f1d7266dd5
Ratul Hasan <ratuljh@gmail.com>
2018-04-12 12:32:10 +0600

    Image scraper

--- image_scrape.py
+++ image_scrape.py
@@ -4,30 +4,38 @@ from PIL import Image
 from io import BytesIO
 import os
 
-search = input("Enter search query: ")
-params = {"q": search}
+def StartSearch():
+    search = input("Enter search query: ")
+    params = {"q": search}
 
-r = requests.get("https://www.bing.com/images/search", params=params)
+    r = requests.get("https://www.bing.com/images/search", params=params)
 
-soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
 
-links = soup.findAll("a", {"class": "thumb"})
+    links = soup.findAll("a", {"class": "thumb"})
 
-if not os.path.exists("./scraped_images/"+search):
-    os.makedirs("./scraped_images/"+search)
-    path = "./scraped_images/"+search + "/"
-else:
-    path = "./scraped_images/"+search + "/"
+    dir_name = "./scraped_images/" + search.replace(" ", "_").lower()
 
-for item in links:
-    img_obj = requests.get(item.attrs["href"])
-    img_link = item.attrs["href"]
-    title = item.attrs["href"].split("/")[-1]
-    try:
-        image = Image.open(BytesIO(img_obj.content))
-        image.save(path + title, image.format)
-        print(title)
-    except ImportError as e:
-        print (e)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    for item in links:
+        try:
+            img_obj = requests.get(item.attrs["href"])
+            img_link = item.attrs["href"]
+            title = item.attrs["href"].split("/")[-1]
+            try:
+                image = Image.open(BytesIO(img_obj.content))
+                image.save("./" + dir_name + "/" + title, image.format)
+                print(title)
+            except:
+                print("Could not save image")
+
+        except:
+            print("Could not requests!")
+
+    StartSearch()
+
+StartSearch()
 
 
